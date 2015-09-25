ce5e099e384a7ad82d90c123c5ff0c2a0d935531
Moritz <moritz.j.egger@gmail.com>
2015-09-25 12:20:12 +0200

    Yeoman angular basic framework

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name femoIopageApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the femoIopageApp
+ */
+angular.module('femoIopageApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name femoIopageApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the femoIopageApp
+ */
+angular.module('femoIopageApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
